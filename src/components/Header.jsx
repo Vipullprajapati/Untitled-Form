4489e3401d7214e05e393f81034dfe92fe43e1bb
Vipul Prajapati <vipulprajapati0070@gmail.com>
2024-12-09 14:33:03 +0000

    Add icon in header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,9 +2,9 @@ import React from "react";
 
 function Header() {
   return (
-    <header className="flex gap-2">
-      <i>ICON</i>
-      <p>Untitled UI</p>
+    <header className="flex gap-2 flex item-center gap-2">
+      <img src="/public/favicon.png" alt="Favicon" className="w-10 h-10" />
+      <p className="text-2xl font-semibold">Untitled UI</p>
     </header>
   );
 }
